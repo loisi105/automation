a6630e564a6a7f8f1507993b3597ea87433762bb
Rainer Loisinger <loisinger.r@gmail.com>
2026-03-18 09:48:28 +0100

    modify skript to send email for every output and also send the complete error output from clone

--- DailyRcloneSync/rclone-cron.sh
+++ DailyRcloneSync/rclone-cron.sh
@@ -1,13 +1,27 @@
 #!/bin/bash
-rclone sync gdrive: ~/gdrive/
 
-# Capture the exit status
-STATUS=$?
-echo "$STATUS"
-if [ $STATUS -ne 0 ]; then
-    (
-      echo "Subject: Alert: Script Failure"
-      echo ""
-      echo "The shell script failed with return value $STATUS at $(date)."
-    ) | /usr/bin/msmtp loisinger.r@gmail.com
+# Probe the remote directory
+MSG_DUMP=rclone lsd gdrive: > /dev/null 2>&1 --log-level ERROR
+PROBE_STATUS=$?
+
+# Run the sync
+rclone sync gdrive: ~/gdrive/ -v
+SYNC_STATUS=$?
+
+# Determine if there is an error or a success
+if [ "$PROBE_STATUS" -ne 0 ] || [ "$SYNC_STATUS" -ne 0 ]; then
+    MAIL_SUBJECT="Error"
+else
+    MAIL_SUBJECT="Success"
 fi
+
+# Send mail
+echo "Sending mail for $MAIL_SUBJECT"
+(
+  echo "Subject: Script $MAIL_SUBJECT"
+  echo ""
+  echo "Probe Status: $PROBE_STATUS"
+  echo "Sync Status: $SYNC_STATUS"
+  echo "Timestamp: $(date)"
+  echo "Message dump: $MSG_DUMP"
+) | /usr/bin/msmtp loisinger.r@gmail.com
